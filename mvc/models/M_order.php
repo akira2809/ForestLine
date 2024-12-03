5ac430b33da2707fffe479fb39d3e5e23d36f714
KuroNTT <156093235+KuroNTT@users.noreply.github.com>
2024-12-03 12:03:02 +0700

    Update M_order.php

--- mvc/models/M_order.php
+++ mvc/models/M_order.php
@@ -6,10 +6,10 @@ class M_order
     {
         $this->conn = $conn;
     }
-    public function add_order($user_id, $user_name, $phone_number, $address, $total_money, $voucher_id = null)
+    public function add_order($user_id, $user_name, $payment_method, $phone_number, $address, $voucher_id = null)
     {
-        $sql = "INSERT INTO orders (user_id,user_name,phone_number,address,total_money,voucher_id) values(?,?,?,?,?,?)";
-        return $this->conn->insert($sql, [$user_id, $user_name, $phone_number, $address, $total_money, $voucher_id]);
+        $sql = "INSERT INTO orders (user_id,user_name,payment_method,phone_number,address,voucher_id) values(?,?,?,?,?,?)";
+        return $this->conn->insert($sql, [$user_id, $user_name, $payment_method, $phone_number, $address, $voucher_id]);
     }
     public function add_order_detail($order_id, $product_variant_id, $quantity, $price)
     {
@@ -23,31 +23,27 @@ class M_order
     }
     public function get_order_all()
     {
-        $sql = "SELECT orders.order_id,orders.user_id,orders.date,orders.total_money,payment_method.payment_method,orders.status,voucher.description as voucher_description,
-        orders.user_name,orders.phone_number, orders.address, orders.voucher_id,product.name, payment_method.status as payment_method_status,
+        $sql = "SELECT orders.order_id,orders.user_id,orders.date,orders.payment_method,orders.status,
+        orders.user_name,orders.phone_number, orders.address, orders.voucher_id,product.name,
         product_color.color_name, product_size.size_name,order_detail.quantity,order_detail.price FROM orders 
         INNER JOIN order_detail ON orders.order_id = order_detail.order_id 
         INNER JOIN product_variant ON product_variant.product_variant_id = order_detail.product_variant_id
         INNER JOIN product ON product_variant.product_id = product.product_id
-        INNER JOIN payment_method ON payment_method.order_id = orders.order_id
         INNER JOIN product_color ON product_variant.color_id = product_color.color_id
         INNER JOIN product_size ON product_variant.size_id = product_size.size_id
-        LEFT JOIN voucher ON voucher.voucher_id = orders.voucher_id
         ORDER BY orders.order_id DESC";
         return $this->conn->getAll($sql);
     }
     public function get_order_by_user_id($user_id)
     {
-        $sql = "SELECT orders.order_id,orders.user_id,orders.date,orders.status,image.image,
-        orders.user_name,orders.phone_number, orders.address, orders.voucher_id,product.name,
-        product_color.color_name, product_size.size_name,order_detail.quantity,order_detail.price,orders.total_money FROM orders 
+        $sql = "SELECT orders.order_id,orders.user_id,orders.date,orders.payment_method,orders.status,order_detail.order_detail_id,
+        orders.user_name, orders.total_money, orders.phone_number, orders.address, orders.voucher_id,product.name,product.product_id,  
+        product_color.color_name, product_size.size_name,order_detail.quantity,order_detail.price, product.main_image FROM orders 
         INNER JOIN order_detail ON orders.order_id = order_detail.order_id 
         INNER JOIN product_variant ON product_variant.product_variant_id = order_detail.product_variant_id
         INNER JOIN product ON product_variant.product_id = product.product_id
         INNER JOIN product_color ON product_variant.color_id = product_color.color_id
         INNER JOIN product_size ON product_variant.size_id = product_size.size_id
-        INNER JOIN image ON image.image_id = product_variant.image_id 
-        INNER JOIN payment_method ON payment_method.order_id = orders.order_id
         WHERE orders.user_id = ? ORDER BY orders.order_id DESC
         ";
         return $this->conn->getAll($sql, [$user_id]);
@@ -56,21 +52,5 @@ class M_order
     {
         $sql = "UPDATE orders SET status = ? WHERE order_id = ?";
         return $this->conn->query($sql, [$status, $order_id]);
-    }
-    function cancel_order($order_id)
-    {
-        $sql = "UPDATE orders SET status = 'Canceled' WHERE order_id = ?";
-        return $this->conn->query($sql, [$order_id]);
-    }
-    function update_payment_status($order_id, $status)
-    {
-        $date = date('Y-m-d H:i:s');
-        $sql = "UPDATE payment_method SET status = ? , update_at = ? WHERE order_id = ?";
-        return $this->conn->update($sql, [$status, $date, $order_id]);
-    }
-    function add_payment_method($order_id, $payment)
-    {
-        $sql = "INSERT INTO payment_method (order_id, payment_method) VALUES (?, ?)";
-        return $this->conn->update($sql, [$order_id, $payment]);
     }
 }
