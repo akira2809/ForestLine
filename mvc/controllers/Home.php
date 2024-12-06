58820be6d92772e1046bc04dbfd2520e2e9a544b
Kinh Kha <khadoan.190809@gmail.com>
2024-12-06 21:28:51 +0700

    s

--- mvc/controllers/Home.php
+++ mvc/controllers/Home.php
@@ -12,7 +12,7 @@ class Home extends Controller
 
 
         $data['product_new'] = $this->model_product->get_product_by_count(4);
-        $data['product_best_view'] = $this->model_product->get_product_best_view(4);
+        // $data['product_best_view'] = $this->model_product->get_product_best_view(4);
         $data['page'] = 'home';
         $this->view('layout/layout_client', $data);
     }
